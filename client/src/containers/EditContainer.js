c40d02f31a8d44b38ea92b6ad31d97a1f5745eb7
anneeb <ahuang852@gmail.com>
2017-07-23 16:07:12 -0400

    basic working

--- client/src/containers/EditContainer.js
+++ client/src/containers/EditContainer.js
@@ -1,12 +1,113 @@
 import React, { Component } from 'react'
-import { Header } from 'semantic-ui-react'
-import ScriptEditor from '../components/ScriptEditor'
+import BlockStyleControls from '../components/edit/BlockStyleControls'
+import InlineStyleControls from '../components/edit/InlineStyleControls'
+import { Editor, EditorState, RichUtils} from 'draft-js'
+import '../../node_modules/draft-js/dist/Draft.css'
+import '../stylesheets/EditContainer.css'
 
 class EditContainer extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+
+    this.focus = () => this.refs.editor.focus();
+    this.onChange = (editorState) => this.setState({editorState});
+
+    this.handleKeyCommand = (command) => this._handleKeyCommand(command);
+    this.onTab = (e) => this._onTab(e);
+    this.toggleBlockType = (type) => this._toggleBlockType(type);
+    this.toggleInlineStyle = (style) => this._toggleInlineStyle(style);
+  }
+
+  _handleKeyCommand(command) {
+    const {editorState} = this.state;
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return true;
+    }
+    return false;
+  }
+
+  _onTab(e) {
+    const maxDepth = 4;
+    this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
+  }
+
+  _toggleBlockType(blockType) {
+    this.onChange(
+      RichUtils.toggleBlockType(
+        this.state.editorState,
+        blockType
+      )
+    );
+  }
+
+  _toggleInlineStyle(inlineStyle) {
+    this.onChange(
+      RichUtils.toggleInlineStyle(
+        this.state.editorState,
+        inlineStyle
+      )
+    );
+  }
+
+  styleMap = {
+    CODE: {
+      backgroundColor: 'rgba(0, 0, 0, 0.05)',
+      fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+      fontSize: 16,
+      padding: 2,
+    },
+  };
+
+  getBlockStyle(block) {
+    switch (block.getType()) {
+      case 'blockquote': return 'RichEditor-blockquote';
+      default: return null;
+    }
+  }
+
+  render() {
+    const {editorState} = this.state;
+
+    // If the user changes block type before entering any text, we can
+    // either style the placeholder or hide it. Let's just hide it now.
+    let className = 'RichEditor-editor';
+    var contentState = editorState.getCurrentContent();
+    if (!contentState.hasText()) {
+      if (contentState.getBlockMap().first().getType() !== 'unstyled') {
+        className += ' RichEditor-hidePlaceholder';
+      }
+    }
+
     return (
-      <div>
-        <ScriptEditor />
+      <div className="RichEditor-root">
+        <div>
+          <BlockStyleControls
+            editorState={editorState}
+            onToggle={this.toggleBlockType}
+          />
+          <InlineStyleControls
+            editorState={editorState}
+            onToggle={this.toggleInlineStyle}
+          />
+        </div>
+        <div>
+          <div className={className} onClick={this.focus}>
+            <Editor
+              blockStyleFn={this.getBlockStyle}
+              customStyleMap={this.styleMap}
+              editorState={editorState}
+              handleKeyCommand={this.handleKeyCommand}
+              onChange={this.onChange}
+              onTab={this.onTab}
+              placeholder="Tell a story..."
+              ref="editor"
+              spellCheck={true}
+            />
+          </div>
+        </div>
       </div>
     );
   }
