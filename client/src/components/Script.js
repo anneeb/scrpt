d6fba80a3ed0d0a9a19fbb8c25ec2e308cb5bcd3
anneeb <ahuang852@gmail.com>
2017-07-28 10:57:04 -0400

    restore script

--- client/src/components/Script.js
+++ client/src/components/Script.js
@@ -1,33 +1,15 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 import { Header } from 'semantic-ui-react'
-import PDFDocument from 'pdfkit'
-import blobStream from 'blob-stream'
 
 class Script extends Component {
-  state = {
-    src: null
-  }
-
-  componentWillMount() {
-    const doc = new PDFDocument
-    const stream = doc.pipe(blobStream())
-    doc.text('hello, world')
-    doc.end()
-    stream.on('finish', function() {
-      this.setState({
-        src: stream.toBlobURL('application/pdf')
-      })
-    })
-  }
-
   render() {
-    const src = this.state.src
-    if (src) {
-      return <iframe title='scrpt' src={src} />
-    } else {
-      return 'loading...'
-    }
+    return (
+      <div>
+        <Header textAlign='center'>
+          Script
+        </Header>
+      </div>
+    )
   }
 }
 
