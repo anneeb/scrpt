1dfa2f6f097eaf4d725e4d3076b1aafa4118cce4
anneeb <ahuang852@gmail.com>
2017-08-03 11:23:11 -0400

    docs on creation

--- client/src/adapters/ScriptsAdapter.js
+++ client/src/adapters/ScriptsAdapter.js
@@ -1,18 +1,15 @@
 import axios from 'axios'
-import { EditorState, convertToRaw } from 'draft-js'
 import cuid from 'cuid'
+import NewScriptJson from './NewScriptJson'
 
 const ROOT_URL = 'http://localhost:3000/api/v1'
 
 class ScriptsAdapter {
   static createScript = ({ title, editor, password, confirmPassword }) => {
-    const contentState = EditorState.createEmpty().getCurrentContent()
-    const rawContentState = convertToRaw(contentState)
-    const json = JSON.stringify(rawContentState)
     const data = {
       script: { title, password, confirmPassword, cuid: cuid() },
       editor: {name: editor},
-      version: {contentState: json}
+      version: {contentState: NewScriptJson}
     }
     return axios.post(`${ROOT_URL}/scripts`, data)
   }
